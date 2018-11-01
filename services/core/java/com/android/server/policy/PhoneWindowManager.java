fe9d283cefa9457f25703b43ae41c58d8e237a9e
Pranav Vashi <neobuddy89@gmail.com>
2018-11-01 21:46:10 +0530

    Add device key action to toggle flashlight [2/3]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2342,6 +2342,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case CLOSE_APP:
                 closeApp();
                 break;
+            case TORCH:
+                Utils.toggleCameraFlash();
+                break;
             default:
                 break;
         }
