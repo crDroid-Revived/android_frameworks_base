7981a710c245cddc9191fe04c5eeaf0088c31952
Matías Hernández <matiashe@google.com>
2024-03-22 14:26:23 +0100

    Resolve message/conversation image Uris with the correct user id
    Bug: 317503801
    Test: atest ExpandableNotificationRowTest
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:3b913c4237993525d2435a2d1082c6af8997168d)
    Merged-In: I11c5b39f2d9d8f0788acab43640a6d4abcd5a179
    Change-Id: I11c5b39f2d9d8f0788acab43640a6d4abcd5a179

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/row/ExpandableNotificationRow.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/row/ExpandableNotificationRow.java
@@ -47,6 +47,7 @@ import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.os.UserHandle;
 import android.service.notification.StatusBarNotification;
 import android.util.ArraySet;
 import android.util.AttributeSet;
@@ -453,6 +454,8 @@ public class ExpandableNotificationRow extends ActivatableNotificationView
     public void setEntry(@NonNull NotificationEntry entry) {
         mEntry = entry;
         mStatusBarNotification = entry.notification;
+        mImageResolver = new NotificationInlineImageResolver(userContextForEntry(mContext, entry),
+                new NotificationInlineImageCache());
         if (mStatusBarNotification != null) {
             updateAlarmOrCall();
             AppLockManager appLockManager = (AppLockManager) mContext
@@ -1660,8 +1663,6 @@ public class ExpandableNotificationRow extends ActivatableNotificationView
         mFalsingManager = Dependency.get(FalsingManager.class);  // TODO: inject into a controller.
         mNotificationInflater = new NotificationContentInflater(this);
         mMenuRow = new NotificationMenuRow(mContext);
-        mImageResolver = new NotificationInlineImageResolver(context,
-                new NotificationInlineImageCache());
         mMediaManager = Dependency.get(NotificationMediaManager.class);
         initDimens();
     }
@@ -1672,6 +1673,14 @@ public class ExpandableNotificationRow extends ActivatableNotificationView
 
     public void setStatusBarStateController(StatusBarStateController statusBarStateController) {
         mStatusbarStateController = statusBarStateController;
+    }
+
+    private static Context userContextForEntry(Context base, NotificationEntry entry) {
+        if (base.getUserId() == entry.notification.getNormalizedUserId()) {
+            return base;
+        }
+        return base.createContextAsUser(
+                UserHandle.of(entry.notification.getNormalizedUserId()), /* flags= */ 0);
     }
 
     private void initDimens() {
