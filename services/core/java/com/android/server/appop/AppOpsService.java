25faf4f1c04b5cbc35c66be69f66de9e2d61bb67
Evan Severson <evanseverson@google.com>
2026-02-05 18:16:55 -0800

    [BACKPORT] Enforce suspend restrictions in startOp and noteOp paths
    Suspended apps could previously bypass AppOps restrictions by using the
    startOp and noteOp code paths, as these did not check the package
    suspended state. This change adds a check for isOpRestrictedDueToSuspend
    in noteOperationUnchecked, startOperationUnchecked.
    
    Refactored FrameworksServicesTests to verify these restrictions. The
    tests now independently check checkOp, noteOp, and startOp paths.
    
    Bug: 477935679
    Test: atest FrameworksServicesTests:SuspendPackagesTest \
              CtsSuspendAppsTestCases:SuspendPackagesTest
    Flag: EXEMPT CVE_FIX
    Change-Id: Ifeac8cebdec2fb8d6129ecfdf18b4413303b62f4

--- services/core/java/com/android/server/appop/AppOpsService.java
+++ services/core/java/com/android/server/appop/AppOpsService.java
@@ -2921,7 +2921,6 @@ public class AppOpsService extends IAppOpsService.Stub {
             Slog.e(TAG, "checkOperation", e);
             return AppOpsManager.opToDefaultMode(code);
         }
-
         if (isOpRestrictedDueToSuspend(code, packageName, uid)) {
             return AppOpsManager.MODE_IGNORED;
         }
@@ -3091,7 +3090,9 @@ public class AppOpsService extends IAppOpsService.Stub {
             Slog.e(TAG, "noteOperation", e);
             return AppOpsManager.MODE_ERRORED;
         }
-
+        if (isOpRestrictedDueToSuspend(code, packageName, uid)) {
+            return AppOpsManager.MODE_IGNORED;
+        }
         if (proxyAttributionTag != null
                 && !isAttributionTagDefined(packageName, proxyPackageName, proxyAttributionTag)) {
             proxyAttributionTag = null;
